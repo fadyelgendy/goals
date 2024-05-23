b6fae8c0797a3bf0711800cad8b8d804fc7e32bf
Fady G Andrawes <fadyelgendy2017@gmail.com>
2024-05-23 11:21:50 +0300

    Goals Models and middlewares

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,20 @@
-console.log('Server');
+const express = require('express');
+const { errorHandler } = require('./middlewares/errorHandlerMiddleware');
+const dotenv = require('dotenv').config();
+const colors = require('colors');
+const connect = require("./config/database");
+const PORT = process.env.PORT || 5000;
+
+connect();
+
+const app = express();
+
+// Middlewares
+app.use(express.json())
+app.use(express.urlencoded({extended: false}));
+
+app.use('/api/goals', require('./routes/goalRoutes'));
+
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`Server Started on PORT: ${PORT}`));
